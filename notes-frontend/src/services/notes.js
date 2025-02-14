a4f5ab8b557db736682f85a921bf847eb820faf8
Suraj Pokhrel <Pokhrel004@gmail.com>
2025-02-14 13:53:00 +0545

    changes

--- notes-frontend/src/services/notes.js
+++ notes-frontend/src/services/notes.js
@@ -1,14 +1,14 @@
 import axios from 'axios'
 const baseUrl = '/api/notes'
 
-const getAll = () => {
-    const request = axios.get(baseUrl)
+const getAll = async () => {
+    const response = await axios.get(baseUrl)
     // const nonExisting = {
     //     id: 10000,
     //     content: 'This note is not saved to server',
     //     important: true,
     // }
-    return request.then(response => response.data)
+    return await response.data
 
 }
 
